3e828a3fd94868991ed574bebd01bd389f2c1619
tnfaid <nurfaidahtitik@gmail.com>
2019-10-26 11:44:47 +0700

    opencv

--- openCVLibrary345/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary345/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: E:\\_Skripsi\\Titik_PantauPadi\\openCVLibrary345\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: E:\\_Skripsi\\App Resistor - titik\\openCVLibrary345\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
